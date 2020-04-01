3c0d5c4967d7e970303dafa617c8a6cf523a6985
mohansethuraman <mohan@reckonsys.com>
2020-04-01 15:43:11 +0530

    Responsiveness
    New updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,17 @@
 window.addEventListener('scroll', function () {
     var topValue = this.scrollY;
-    if (document.title == "Team") {
+    if (document.title == "Team" || document.title=="Portfolio" || document.title=="Contact" ) {
 
     }
     else {
         addclassOnNav(topValue);
     }
+});
+
+document.querySelector('.mobile-menu').addEventListener('click',function(){
+    this.classList.toggle('open');
+    document.querySelector('nav .right').classList.toggle('open');
+
 });
 function addclassOnNav(scrollValue) {
     if (scrollValue > 60) {
@@ -17,9 +23,16 @@ function addclassOnNav(scrollValue) {
 
 }
 
-function pullDown() {
-    window.scrollTo()
+function  openModal(){
+    document.querySelector('.modal').classList.add('open');
+    document.querySelector('body').style.overflow="hidden";
 }
+
+function closeModal(){
+    document.querySelector('.modal').classList.remove('open');
+    document.querySelector('body').style.overflow="auto";
+}
+
 
 // blog slider
 var blogSliderCount= 0;
@@ -43,9 +56,11 @@ function changeSlide(count){
     for(i=0;i<slidesCount;i++){
         if(i==count){
             slides[i].classList.add('active');
+            blogLines[i].classList.add('active');
         }
         else{
             slides[i].classList.remove('active');
+            blogLines[i].classList.remove('active');
         }
     }
 }
@@ -62,6 +77,24 @@ function AutoBlogSlider(){
         
 }
 var blogSlider = setInterval(AutoBlogSlider,6000);
+
+function showDelAddress(el){
+    el.classList.add('active');
+    document.getElementById('del-address').style.display="block";
+    document.getElementById('ban-address').style.display="none";
+    document.getElementById('delMap').style.display="inline-block";
+    document.getElementById('banMap').style.display="none";
+    document.querySelector('.tabs .tab:first-child').classList.remove('active');
+}
+function showBanAddress(el){
+    el.classList.add('active');
+    document.getElementById('del-address').style.display="none";
+    document.getElementById('ban-address').style.display="block";
+    document.getElementById('delMap').style.display="none";
+    document.getElementById('banMap').style.display="inline-block";
+    document.querySelector('.tabs .tab:nth-child(2)').classList.remove('active');
+}
+
 
 
 
